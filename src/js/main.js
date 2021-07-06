a535c799b2623b8136a258a062a1c7c9f1ed28ee
Amir <amirkadenov@gmail.com>
2021-07-06 13:24:41 +0600

    finished js section range -> finished js section styles ->  finished js section

--- src/js/main.js
+++ src/js/main.js
@@ -153,93 +153,100 @@ document.addEventListener('DOMContentLoaded', () => {
 		const onePercent = target / 100;
 		return (target - (target - val)) / onePercent;
 	};
+	const delay = (fn, ms) => {
+		return function() {
+			if (Date.now() < delay.nextPossibleCallDate) {
+				return;
+			}
+			fn(...arguments);
+			delay.nextPossibleCallDate = Date.now() + ms;
+		};
 
+	};
 	const getFromPercentages = (percentages, target) => {
 		return target / 100 * percentages;
 	};
 
-	const isCloserToUpper = (curr, upperValue, lowerValue) => {
-		let lowerCount, higherCount
-		higherCount = lowerCount = 0
-		for (let i = curr; i <= upperValue; i++,higherCount++) {}
-		for (let i = curr; i >= lowerValue; i--,lowerCount++) {}
-		return higherCount > lowerCount
-	};
-
-
-	getDashesAmount = (point) => {
-		const points =new Map([
-			[ 0, {dashes: 7, markerHeight: 0}],
-			[ 25, {dashes:6, markerHeight: 6}],
-			[ 50, {dashes:5, markerHeight: 9}],
-			[ 75, {dashes:4, markerHeight: 11}],
-			[ 100, {dashes:3, markerHeight: 14}],
-		]);
-		console.log(points, point)
-		return (points.get(Number(point)))
+	getDashesAmount = (point, points) => {
+		point = Number(point)
+		const v = points.get(point)
+		if (v) {
+			points.set(point, {...v, isUsed : true})
+		}
+		return v;
 	};
 
 	const handleJSLevelRange = () => {
+
+		const points = new Map([
+			[0, {dashes: 7, markerHeight: 0}],
+			[24, {dashes: 6, markerHeight: 6}],
+			[50, {dashes: 5, markerHeight: 9}],
+			[75, {dashes: 4, markerHeight: 11}],
+			[100, {dashes: 3, markerHeight: 14}],
+		]);
+
 		const range = document.querySelector('[data-range]');
 		const rangeControl = range.querySelector('.range__control');
 		const rangeControlContainer = range.querySelector(
 			'.range__control-container');
 		const rangeGradients = document.querySelectorAll('.range__gradient');
+		const rangeGradientsFills = document.querySelectorAll(
+			'.range__gradient-fill');
 		const initialValuePercentages = range.dataset.rangeStart;
 		const rangeWidth = range.offsetWidth;
 
-		const createPoints = () => {
+		const createPoints = (pointsData) => {
 			const points = range.querySelectorAll('[data-point]');
 			points.forEach(point => {
-				let orientationAdditionClass = ''
-				let textAdditionClass = ''
-				let markerAdditionalClass = ''
+				let orientationAdditionClass = '';
+				let textAdditionClass = '';
+				let markerAdditionalClass = '';
 				let offsetLeft = getFromPercentages(point.dataset.point,
 					rangeWidth);
 				if (offsetLeft === rangeWidth) {
-					point.classList.add('range__point--right')
-				    orientationAdditionClass = 'range__point-orientation--right'
-					textAdditionClass = 'range__point-text--right'
-					markerAdditionalClass = 'range__point-marker--right'
+					point.classList.add('range__point--right');
+					orientationAdditionClass = 'range__point-orientation--right';
+					textAdditionClass = 'range__point-text--right';
+					markerAdditionalClass = 'range__point-marker--right';
 				} else {
 					point.style.left = offsetLeft + 'px';
 				}
 
 				const orientation = document.createElement('div');
-				orientation.className = 'range__point-orientation ' + orientationAdditionClass;
+				orientation.className = 'range__point-orientation ' +
+					orientationAdditionClass;
 
-				const {dashes: dashesAmount, markerHeight} = getDashesAmount(point.dataset.point);
+				const {dashes: dashesAmount, markerHeight} = getDashesAmount(
+					point.dataset.point, pointsData);
 				const dashes = document.createElement('div');
 				dashes.className = 'range__point-dashes';
-				dashes.style.height = dashesAmount * 2 + 'px'
+				dashes.style.height = dashesAmount * 2 + 'px';
 				for (let i = 0; i < dashesAmount; i++) {
-					const dash = document.createElement('span')
-					dash.className = 'range__point-dash'
-					dashes.append(dash)
+					const dash = document.createElement('span');
+					dash.className = 'range__point-dash';
+					dashes.append(dash);
 				}
-				orientation.append(dashes)
+				orientation.append(dashes);
 
 				const marker = document.createElement('div');
-				marker.className = 'range__point-marker ' + markerAdditionalClass;
-				marker.style.height = markerHeight + 'px'
-				orientation.append(marker)
+				marker.className = 'range__point-marker ' +
+					markerAdditionalClass;
+				marker.style.height = markerHeight + 'px';
+				orientation.append(marker);
 
-				const text = document.createElement('p')
-				text.className = 'range__point-text ' + textAdditionClass
-				text.textContent = point.dataset.text
-				point.append(text)
-				point.append(orientation)
+				const text = document.createElement('p');
+				text.className = 'range__point-text ' + textAdditionClass;
+				text.textContent = point.dataset.text;
+				point.append(text);
+				point.append(orientation);
 			});
 		};
-		createPoints();
+		createPoints(points);
 
 		const setValues = (direction, v, step) => {
-			rangeGradients.forEach(gradient => {
-				if (direction === 'up') {
-					gradient.style.width = v + 'px';
-				} else if (direction === 'down') {
-					gradient.style.width = v + 'px';
-				}
+			rangeGradientsFills.forEach(fill => {
+				fill.style.width = rangeWidth - v + 'px';
 			});
 
 			if (direction === 'up') {
@@ -252,60 +259,122 @@ document.addEventListener('DOMContentLoaded', () => {
 			}
 		};
 
-		const changeValues = (e, step = 1, ms = 50) => {
-			const coords = e.target.getBoundingClientRect();
-			const v = e.pageX - coords.x;
+		const changeValues = (e, el, step = 1, ms = 50, absaluteValue = null) => {
+			let v;
+			// if value is provided then just move to it
+			if (absaluteValue !== null) {
+				v = absaluteValue
+			} else {
+				const coords = el.getBoundingClientRect();
+				v = e.pageX - coords.x;
+			}
+
 			const goal = v;
 			const initial = rangeControl.style.left.replace('px', '');
 			const direction = goal > initial ? 'up' : 'down';
 
-			let i = setInterval(() => {
+			let i = setTimeout(function handler() {
 				setValues(direction, v, step);
 				const current = v;
 				if (current === goal) {
-					clearInterval(i);
-					return current;
+					clearTimeout(i);
+					mouseDownHandler.current = current;
+				} else {
+					i = setTimeout(handler, ms)
 				}
+
 			}, ms);
 
 		};
-		const delay = (fn, ms) => {
-			return function() {
-				if (Date.now() < delay.nextPossibleCallDate) {
-					return;
-				}
-				fn(...arguments);
-				delay.nextPossibleCallDate = Date.now() + ms;
-			};
 
+
+		const isCloserToUpper = (curr, upperValue, lowerValue) => {
+			let lowerCount, higherCount;
+			higherCount = lowerCount = 0;
+			for (let i = curr; i <= upperValue; i++, higherCount++) {}
+			for (let i = curr; i >= lowerValue; i--, lowerCount++) {}
+			return higherCount > lowerCount;
 		};
+
+		const moveToNearestPoint = (e, curr, points) => {
+
+			// push all offset values into array
+			const diffArray = [];
+			points.reduce((previous, current) => {
+				let currentDifference =  current - curr;
+				diffArray.push({
+					value: currentDifference,
+					absValue: Math.abs(currentDifference),
+					point: current,
+				});
+				return current;
+			}, points[ 0 ]);
+
+			// find min offset
+			let min;
+			diffArray.forEach(item => {
+				if ( min === undefined
+					|| item.absValue < min.absValue ) min = item;
+			});
+			setTimeout(() => changeValues(null, null, 1, 400, min.point),200)
+		};
+
+		const assignValuesAccordingToPercentages = (arr, width) => {
+			return [...arr].map(i => getFromPercentages(i, width));
+		};
+		const removeUnusedElements = (points) => {
+			for (const entry of points) {
+				if (entry[1].isUsed === undefined) {
+					points.delete(entry[0])
+				}
+			}
+			return points
+		}
+
 		const mouseDownHandler = function(e) {
-			changeValues(e);
 			const mouseMoveHandler = (e) => {
-				changeValues(e);
+				changeValues(e, rangeControlContainer);
 			};
 			const mouseUpHandler = (e) => {
-				range.removeEventListener('mousemove', mouseMoveHandler);
-				setTimeout(
-					() => range.removeEventListener('mouseup', mouseUpHandler));
+				rangeControlContainer.removeEventListener('mousemove',
+					mouseMoveHandler);
+				setTimeout(() => {
+					rangeControlContainer.removeEventListener('mouseup',
+						mouseUpHandler);
+					moveToNearestPoint(e, mouseDownHandler.current,
+						assignValuesAccordingToPercentages(removeUnusedElements(points).keys(),
+							rangeWidth));
+				});
 			};
 
 			const mouseOutHandler = (e) => {
-				range.removeEventListener('mousemove', mouseMoveHandler);
-				range.removeEventListener('mouseup', mouseUpHandler);
+				setTimeout(() => {
+					if (e.target === range) {
+						rangeControlContainer.removeEventListener('mousemove',
+							mouseMoveHandler);
+					}
+					// rangeControlContainer.removeEventListener('mouseup', mouseUpHandler);
+
+				});
 			};
 			range.addEventListener('mouseout', mouseOutHandler);
-			range.addEventListener('mousemove', mouseMoveHandler);
-			range.addEventListener('mouseup', mouseUpHandler);
+			rangeControlContainer.addEventListener('mousemove',
+				mouseMoveHandler);
+			rangeControlContainer.addEventListener('mouseup', mouseUpHandler);
 		};
 
-		range.addEventListener('mousedown', mouseDownHandler);
+		rangeControl.addEventListener('mousedown', mouseDownHandler);
 
-		const initialPosition = getFromPercentages(initialValuePercentages,
-			rangeWidth);
-		rangeControl.style.setProperty('left', initialPosition + 'px');
-		rangeGradients.forEach(
-			(item) => item.style.setProperty('width', initialPosition + 'px'));
+		const setInititalPosition = () => {
+			const initialPosition = getFromPercentages(initialValuePercentages,
+				rangeWidth);
+			rangeControl.style.setProperty('left', initialPosition + 'px');
+			rangeGradientsFills.forEach(
+				(item) => item.style.setProperty('width',
+					initialPosition + 'px'));
+		};
+		setInititalPosition();
+
 	};
 	handleJSLevelRange();
 });
